d565f98a865c44949a1185b3b07b51cc5902145d
conmeobeo121 <kiendeptrai2002@gmail.com>
2024-11-25 21:11:23 +0700

    push something...

--- src/main/java/com/example/quizcards/repository/ISetFlashcardRepository.java
+++ src/main/java/com/example/quizcards/repository/ISetFlashcardRepository.java
@@ -50,7 +50,7 @@ public interface ISetFlashcardRepository extends JpaRepository<SetFlashcard, Lon
 //            """, nativeQuery = true)
     @Query(value = """
             SELECT s.set_id, s.title, s.description_set, s.created_at, s.updated_at, s.is_approved, s.is_anonymous, s.sharing_mode,
-                  a.last_name, a.first_name, a.user_name, a.avatar, c.category_name, COUNT(DISTINCT f.card_id) AS total_card
+                  a.last_name, a.first_name, a.user_name, a.user_id, a.avatar, c.category_name, COUNT(DISTINCT f.card_id) AS total_card
            FROM set_flashcards s
            JOIN app_users a ON s.user_id = a.user_id
            JOIN category_set_flashcards c ON s.category_id = c.category_id
