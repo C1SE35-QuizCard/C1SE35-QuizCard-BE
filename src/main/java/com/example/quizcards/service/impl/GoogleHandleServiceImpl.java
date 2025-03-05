fda07020820fd3642030d86c9f2a994273ed9853
conmeobeo121 <kiendeptrai2002@gmail.com>
2025-03-05 08:25:03 +0700

    change using FeignClient to call Gmail API

--- src/main/java/com/example/quizcards/service/impl/GoogleHandleServiceImpl.java
+++ src/main/java/com/example/quizcards/service/impl/GoogleHandleServiceImpl.java
@@ -1,96 +1,61 @@
 package com.example.quizcards.service.impl;
 
+import com.example.quizcards.client.GoogleOAuth2Client;
+import com.example.quizcards.client.GoogleUserInfoClient;
 import com.example.quizcards.dto.GoogleInfoUser;
 import com.example.quizcards.dto.request.GoogleLoginRequest;
 import com.example.quizcards.dto.response.GoogleAccessTokenResponse;
 import com.example.quizcards.dto.response.GoogleUserInfoResponse;
 import com.example.quizcards.service.IGoogleHandleService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import org.springframework.util.MultiValueMap;
 
 @Service
 public class GoogleHandleServiceImpl implements IGoogleHandleService {
-    private ClientRegistrationRepository clientRegistrationRepository;
+    private final ClientRegistration clientRegistration;
+    private final GoogleOAuth2Client googleOAuth2Client;
+    private final GoogleUserInfoClient googleUserInfoClient;
 
-    private ClientRegistration clientRegistration = null;
-
-    public GoogleHandleServiceImpl(ClientRegistrationRepository repository) {
-        clientRegistrationRepository = repository;
-        clientRegistration = clientRegistrationRepository.findByRegistrationId("google");
+    public GoogleHandleServiceImpl(ClientRegistrationRepository clientRegistrationRepository,
+                                   GoogleOAuth2Client googleOAuth2Client,
+                                   GoogleUserInfoClient googleUserInfoClient) {
+        this.googleOAuth2Client = googleOAuth2Client;
+        this.googleUserInfoClient = googleUserInfoClient;
+        this.clientRegistration = clientRegistrationRepository.findByRegistrationId("google");
     }
 
-    private Map<String, String> getGoogleOAuth2Params(String code) {
-        String clientId = clientRegistration.getClientId();
-        String clientSecret = clientRegistration.getClientSecret();
+    private MultiValueMap<String, String> getGoogleOAuth2Params(String code) {
+        MultiValueMap<String, String> params = new org.springframework.util.LinkedMultiValueMap<>();
+        params.add("code", code);
+        params.add("client_id", clientRegistration.getClientId());
+        params.add("client_secret", clientRegistration.getClientSecret());
 
-        Map<String, String> params = new HashMap<>();
-        params.put("code", code);
-        params.put("client_id", clientId);
-        params.put("client_secret", clientSecret);
-        params.put("redirect_uri", "postmessage");
-        params.put("grant_type", "authorization_code");
-
+        params.add("redirect_uri", "postmessage");
+        params.add("grant_type", "authorization_code");
         return params;
     }
 
-    private String getAccessTokenFromCode(String code) throws JsonProcessingException {
-        String tokenUri = clientRegistration.getProviderDetails().getTokenUri();
-
-        Map<String, String> params = getGoogleOAuth2Params(code);
-
-        StringBuilder requestBody = new StringBuilder();
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            if (!requestBody.isEmpty()) {
-                requestBody.append("&");
-            }
-            requestBody.append(entry.getKey()).append("=").append(entry.getValue());
-        }
-
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody.toString(), headers);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(tokenUri, requestEntity, String.class);
-
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            ObjectMapper mapper = new ObjectMapper();
-
-            GoogleAccessTokenResponse response = mapper.readValue(responseEntity.getBody(), GoogleAccessTokenResponse.class);
-
-            return response.getAccess_token();
-        }
-
-        return null;
+    private String getAccessTokenFromCode(String code) {
+        MultiValueMap<String, String> params = getGoogleOAuth2Params(code);
+        GoogleAccessTokenResponse tokenResponse = googleOAuth2Client.getAccessToken(params);
+        return tokenResponse.getAccess_token();
     }
 
-    private GoogleUserInfoResponse getInfoUserFromAccessToken(String accessToken) throws JsonProcessingException {
-        String userInfoUri = clientRegistration.getProviderDetails().getUserInfoEndpoint().getUri();
+    private String getAccessTokenFromCode_2(String code) {
+        GoogleAccessTokenResponse tokenResponse = googleOAuth2Client.getAccessToken_2(
+                code,
+                clientRegistration.getClientId(),
+                clientRegistration.getClientSecret(),
+                "postmessage",
+                "authorization_code"
+        );
+        return tokenResponse.getAccess_token();
+    }
 
-        String urlUserInfoBody = userInfoUri + "?access_token=" + accessToken;
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        ResponseEntity<String> userDataJsonString = restTemplate.getForEntity(urlUserInfoBody, String.class);
-
-        if (userDataJsonString.getStatusCode().is2xxSuccessful()) {
-            ObjectMapper mapper = new ObjectMapper();
-
-            return mapper.readValue(userDataJsonString.getBody(), GoogleUserInfoResponse.class);
-        }
-
-        return null;
+    private GoogleUserInfoResponse getInfoUserFromAccessToken(String accessToken) {
+        return googleUserInfoClient.getUserInfo(accessToken);
     }
 
     @Override
@@ -100,26 +65,26 @@ public class GoogleHandleServiceImpl implements IGoogleHandleService {
         }
 
         String code = request.getCode();
+        GoogleUserInfoResponse userResponse = getInfoUserFromAccessToken(getAccessTokenFromCode_2(code));
 
-        GoogleUserInfoResponse user = getInfoUserFromAccessToken(getAccessTokenFromCode(code));
+        GoogleInfoUser userInfo = GoogleInfoUser
+                .builder()
+                .email(userResponse.getEmail())
+                .avatarUrl(userResponse.getPicture())
+                .firstName(userResponse.getGiven_name())
+                .lastName(userResponse.getFamily_name())
+                .userCode(userResponse.getSub())
+                .build();
 
-        GoogleInfoUser userInfo = new GoogleInfoUser();
+        int atIndex = userResponse.getEmail().indexOf("@");
 
-        assert user != null;
-        userInfo.setEmail(user.getEmail());
-        userInfo.setAvatarUrl(user.getPicture());
-        userInfo.setFirstName(user.getGiven_name());
-        userInfo.setLastName(user.getFamily_name());
-        userInfo.setUserCode(user.getSub());
-
-        int atIndex = user.getEmail().indexOf("@");
         if (atIndex != -1) {
-            userInfo.setUserName(user.getEmail().substring(0, atIndex));
+            userInfo.setUserName(userResponse.getEmail().substring(0, atIndex));
         } else {
-            userInfo.setUserName(user.getEmail());
+            userInfo.setUserName(userResponse.getEmail());
         }
 
-        userInfo.setEnabled(user.isEmail_verified());
+        userInfo.setEnabled(userResponse.isEmail_verified());
 
         return userInfo;
     }
