9847396b64b5c9afb32e5979dccd79455c1e3155
conmeobeo121 <kiendeptrai2002@gmail.com>
2025-04-10 01:15:30 +0700

    - Tinh chỉnh + Phần set timezone với thời gian trong StreakRequest.java: Thêm cấu hình thủ công, không sử dụng cấu hình mặc định để tránh chuyển về UTC khi từ client chuyển đến server

--- src/main/java/com/example/quizcards/dto/request/StreakRequest.java
+++ src/main/java/com/example/quizcards/dto/request/StreakRequest.java
@@ -1,11 +1,14 @@
 package com.example.quizcards.dto.request;
 
 import com.example.quizcards.validation.ValidOffset;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @Builder
@@ -13,7 +16,25 @@ import java.time.OffsetDateTime;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StreakRequest {
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+//    @NotNull
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+//    @ValidOffset
+//    OffsetDateTime timeFromClient;
+
+    @NotNull
     @ValidOffset
     OffsetDateTime timeFromClient;
+
+    @JsonSetter("timeFromClient")
+    public void setTimeFromClient(String timeStr) {
+        this.timeFromClient = OffsetDateTime.parse(timeStr, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+
+    @JsonGetter("timeFromClient")
+    public String getTimeFromClientAsString() {
+        if (timeFromClient != null) {
+            return timeFromClient.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+        return null; // Không xảy ra vì @NotNull đảm bảo không null
+    }
 }
