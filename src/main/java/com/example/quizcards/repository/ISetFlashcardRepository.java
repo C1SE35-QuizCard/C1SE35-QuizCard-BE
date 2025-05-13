21db38d5e970c2410aaf4161677c79c5f09d29d6
TrangThanhAn <thanhannbk0912@gmail.com>
2025-05-13 17:52:40 +0700

    Update: File ISetFlashcardRepository add ufs.last_accessed to group by of findAllSetPublicNearbySettings to run

--- src/main/java/com/example/quizcards/repository/ISetFlashcardRepository.java
+++ src/main/java/com/example/quizcards/repository/ISetFlashcardRepository.java
@@ -548,7 +548,7 @@ public interface ISetFlashcardRepository extends JpaRepository<SetFlashcard, Lon
             JOIN flashcards f ON f.set_id = s.set_id
             JOIN user_flashcard_settings ufs on s.set_id = ufs.set_id
             WHERE s.user_id = :user_id
-            GROUP BY s.set_id, s.title, s.description_set, s.created_at, s.updated_at, s.is_approved, s.is_anonymous, s.sharing_mode, a.first_name, a.last_name, a.user_name, a.user_id, a.avatar, c.category_name, s.hash_password
+            GROUP BY s.set_id, s.title, s.description_set, s.created_at, s.updated_at, s.is_approved, s.is_anonymous, s.sharing_mode, a.first_name, a.last_name, a.user_name, a.user_id, a.avatar, c.category_name, s.hash_password, ufs.last_accessed
             ORDER BY ufs.last_accessed desc
             LIMIT :limit
             """, nativeQuery = true)
