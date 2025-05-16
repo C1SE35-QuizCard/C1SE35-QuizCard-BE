426e55864115e1fc62615803dad15571c8c9cda9
conmeobeo121 <kiendeptrai2002@gmail.com>
2025-05-16 17:02:23 +0700

    updated code

--- src/main/java/com/example/quizcards/config/SecurityConfig.java
+++ src/main/java/com/example/quizcards/config/SecurityConfig.java
@@ -111,8 +111,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)  // Sử dụng phương pháp mới để vô hiệu hóa CSRF
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//                .csrf(AbstractHttpConfigurer::disable)  // Sử dụng phương pháp mới để vô hiệu hóa CSRF
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(authEndpoints).authenticated()
