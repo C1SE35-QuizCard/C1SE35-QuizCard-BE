be23ab3b1f81d31d498812e6aa7394bf779710c9
conmeobeo121 <kiendeptrai2002@gmail.com>
2025-05-20 09:02:48 +0700

    fixed bug

--- src/main/java/com/example/quizcards/repository/ISetFlashcardRepository.java
+++ src/main/java/com/example/quizcards/repository/ISetFlashcardRepository.java
@@ -237,7 +237,7 @@ public interface ISetFlashcardRepository extends JpaRepository<SetFlashcard, Lon
         """,
             nativeQuery = true
     )
-    List<ISetFlashcardDTO> findSetCardByUserId(@Param("userId") Long userId);
+    List<ISetFlashcardDTO> findSetCardByUserId(@Param("userIdm ") Long userId);
 
 
     @Query(
